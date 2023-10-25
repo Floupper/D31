1084deed03e8a60427c344a50562a95a6e24a911
Mathieu Ackermann <mathieu.ackermann@etu.unistra.fr>
2023-10-25 15:20:23 +0200

    Push TP5 Final Part

--- TP5/TP5.sql
+++ TP5/TP5.sql
@@ -121,6 +121,106 @@ CREATE OR REPLACE PACKAGE GestionFinanciere AS
 END GestionFinanciere;
 /
 
+-- Question 14
+CREATE OR REPLACE PACKAGE BODY GestionFinanciere AS
+    CREATE PROCEDURE augmentMontant(employe IN emp.empno%TYPE, montant IN emp.sal%TYPE)
+    IS
+    BEGIN
+        UPDATE emp SET sal = sal + montant WHERE ename = employe;
+        -- Fin de la transaction
+        COMMIT;
+    END;
+
+    CREATE PROCEDURE afficheSalEmp(employe IN emp.empno%TYPE)
+    IS
+        salaire emp.sal%TYPE;
+    BEGIN
+        SELECT sal INTO salaire FROM emp WHERE ename = employe;
+    END;
+
+    CREATE PROCEDURE annuaire
+    IS 
+    BEGIN
+        FOR emp IN (SELECT ename, job, dname FROM emp JOIN dept ON emp.deptno = dept.deptno)
+        LOOP 
+            DBMS_OUTPUT.PUT_LINE(emp.ename || ' - ' || emp.job || ' - ' || emp.dname);
+        END LOOP;
+    END;
+
+    CREATE PROCEDURE augmentPourcent(employe IN emp.empno%TYPE, pourcent IN DECIMAL(5,2), nouvSal IN emp.sal%TYPE)
+    IS
+    BEGIN 
+        -- Déclaration des variables
+        DECLARE salaireActuel DECIMAL(10, 2);
+
+        -- Obtenir le salaire actuel de l'employé
+        SELECT sal INTO salaireActuel FROM emp WHERE empno = employe;
+
+        -- Calculer le nouveau salaire avec l'augmentation en pourcentage
+        SET nouvSal = salaireActuel + (salaireActuel * pourcent / 100);
+
+        -- Mettre à jour le salaire de l'employé
+        UPDATE emp SET sal = nouvSal WHERE empno = employe;
+        
+        -- Fin de la transaction
+        COMMIT;
+    END;
+
+    CREATE FUNCTION revenuAnnuel(employe IN emp.empno%TYPE) RETURNS DECIMAL(10, 2)
+    IS
+    BEGIN
+        DECLARE salaire DECIMAL(8, 2);
+        DECLARE commission DECIMAL(8, 2);
+        
+        -- Récupérer le salaire de l'employé
+        SELECT sal INTO salaire FROM emp WHERE empno = employe;
+        
+        -- Récupérer la commission de l'employé
+        SELECT comm INTO commission FROM emp WHERE empno = employe;
+        
+        -- Si la commission est nulle, retourner seulement le salaire annuel
+        IF commission IS NULL THEN
+            RETURN salaire * 12;
+        ELSE
+            -- Si la commission existe, ajouter la commission au salaire et retourner le total annuel
+            RETURN (salaire + commission) * 12;
+        END IF;
+    END;
+
+    CREATE FUNCTION revenuAnnuelPromoAnc(employe IN emp.empno%TYPE) RETURNS DECIMAL(10, 2)
+    IS 
+    BEGIN
+        DECLARE salaire DECIMAL(8, 2);
+        DECLARE exp DECIMAL(8, 2);
+
+        SELECT sal INTO salaire FROM emp WHERE empno = employe;
+        SELECT (extract(year from SYSDATE) - extract(year from hiredate)) INTO exp FROM emp WHERE empno = employe;
+
+        IF exp < 10 THEN
+            RETURN salaire * 12 + salaire;
+        ELSE
+            RETURN salaire * 12 * (1 + exp / 100);
+        END IF;
+    END;
+
+    CREATE FUNCTION revenuAnnuelPromoAnc(employe IN emp.empno%TYPE) RETURNS DECIMAL(10, 2)
+    IS 
+    BEGIN
+        DECLARE salaire DECIMAL(8, 2);
+        DECLARE exp DECIMAL(8, 2);
+
+        SELECT sal INTO salaire FROM emp WHERE empno = employe;
+        SELECT (extract(year from SYSDATE) - extract(year from hiredate)) INTO exp FROM emp WHERE empno = employe;
+
+        IF exp < 10 THEN
+            RETURN salaire * 12 + salaire;
+        ELSE
+            RETURN salaire * 12 * (1 + exp / 100);
+        END IF;
+    END;
+END GestionFinanciere;
+/
+
 -- Question 16
 CREATE TABLE mes_augmentations(
     empno NUMBER(5) NOT NULL,
